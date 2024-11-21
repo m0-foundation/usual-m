281e93ce5998f067787217eee301716d81ebd456
toninorair <antonina.norair@gmail.com>
2024-11-20 23:35:00 -0500

    Oracle proxy clean-ups

--- src/oracle/NAVProxyUsualMPriceFeed.sol
+++ src/oracle/NAVProxyUsualMPriceFeed.sol
@@ -5,21 +5,21 @@ pragma solidity 0.8.26;
 import { AggregatorV3Interface } from "./AggregatorV3Interface.sol";
 
 /**
- * @title  NAV Proxy SmartM Chainlink Compatible Price Feed
+ * @title  NAV Proxy UsualM Chainlink Compatible Price Feed
  * @notice A proxy contract that retrieves NAV (Net Asset Value) data from an external oracle,
- *         converts it to a Smart M price based on a specified threshold, and implements
- *         the Chainlink AggregatorV3Interface for compatibility.
+ *         converts it to a Usual M price based on a specified threshold, and is compatible with
+ *         the Chainlink AggregatorV3Interface.
  * @author M^0 Labs
  */
 contract NAVProxySmartMPriceFeed is AggregatorV3Interface {
-    /// @notice NAV price threshold that defines 1$ Smart M price
+    /// @notice NAV price threshold that defines 1$ UsualM price
     int256 public constant NAV_POSITIVE_THRESHOLD = 1e6;
 
     /// @notice The address of the NAV Oracle from which NAV data is fetched.
     address public immutable navOracle;
 
     /**
-     * @notice Constructs the NAV Proxy Smart M Price Feed contract.
+     * @notice Constructs the NAV Proxy UsualM Price Feed contract.
      * @param  navOracle_ The address of the NAV Oracle.
      */
     constructor(address navOracle_) {
@@ -27,17 +27,17 @@ contract NAVProxySmartMPriceFeed is AggregatorV3Interface {
     }
 
     /// @inheritdoc AggregatorV3Interface
-    function decimals() public view returns (uint8) {
+    function decimals() public pure returns (uint8) {
         return 6;
     }
 
     /// @inheritdoc AggregatorV3Interface
-    function description() external view returns (string memory) {
-        return "NAV Proxy Smart M Price Feed";
+    function description() external pure returns (string memory) {
+        return "M^0 NAV Proxy UsualM Price Feed";
     }
 
     /// @inheritdoc AggregatorV3Interface
-    function version() external view returns (uint256) {
+    function version() external pure returns (uint256) {
         return 1;
     }
 
@@ -53,7 +53,7 @@ contract NAVProxySmartMPriceFeed is AggregatorV3Interface {
             roundId_
         );
 
-        // Convert NAV price to Smart M price given predefined threshold.
+        // Convert NAV price to UsualM price given predefined threshold.
         answer = _getPriceFromNAV(answer);
     }
 
@@ -70,11 +70,11 @@ contract NAVProxySmartMPriceFeed is AggregatorV3Interface {
     }
 
     /**
-     * @dev Converts NAV price to Smart M price based on a predefined sensitivity threshold.
+     * @dev Converts NAV price to UsualM price based on a predefined sensitivity threshold.
      * @param  answer The NAV price to convert.
-     * @return        The Smart M price.
+     * @return        The UsualM price.
      */
-    function _getPriceFromNAV(int256 answer) internal view returns (int256) {
+    function _getPriceFromNAV(int256 answer) internal pure returns (int256) {
         return (answer >= NAV_POSITIVE_THRESHOLD) ? int256(10 ** decimals()) : answer;
     }
 }
